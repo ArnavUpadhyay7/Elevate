9d132c36053517331ea9e7de5411508562722de2
ArnavUpadhyay7 <arnavupadhyay7@gmail.com>
2025-01-27 01:46:48 +0530

    Messages page init

---
+++ frontend/src/components/ChatSidebar.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react";
+import { coachStore, playerStore } from "../store/authStore";
+import { axiosInstance } from "../lib/axios";
+// import { useChatStore } from "../store/useChatStore";
+// import { useAuthStore } from "../store/useAuthStore";
+// import SidebarSkeleton from "./skeletons/SidebarSkeleton";
+import { Users } from "lucide-react";
+
+const ChatSidebar = () => {
+  const [myCoaches, setMyCoaches] = useState([]);
+  const [myPlayers, setMyPlayers] = useState([]);
+  //   const [loading, setLoading] = useState(true);
+  const coachX = coachStore((state) => state.coach);
+  const playerX = playerStore((state) => state.player);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // setLoading(true);
+        if (playerX) {
+          const coachesRes = await axiosInstance.get("/player/my-coaches");
+          setMyCoaches(coachesRes.data);
+        }
+        if (coachX) {
+          const playersRes = await axiosInstance.get("/coach/my-players");
+          console.log(playersRes.data);
+          setMyPlayers(playersRes.data);
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+      //   finally {
+      //     setLoading(false);
+      //   }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <aside className="px-2 h-full w-20 lg:w-72 border-r border-base-300 flex flex-col">
+      <div className="border-b border-base-300 w-full p-5">
+        <div className="flex items-center gap-2">
+          <Users className="size-6" />
+          <span className="font-medium hidden lg:block">
+            {playerX ? "Coaches" : "Players"}
+          </span>
+        </div>
+      </div>
+
+      <div className="overflow-y-auto w-full py-3">
+        {playerX
+          ? myCoaches.map((coach) => (
+              <button
+                key={coach._id}
+                // onClick={() => setSelectedUser(user)}
+                className={`
+                w-full p-3 flex items-center gap-4
+                `}
+                // ${selectedUser?._id === coach._id ? "bg-base-300" : ""}
+              >
+                <div className="relative mx-auto lg:mx-0">
+                  <img
+                    src={coach.profilePic}
+                    alt={coach.fullname}
+                    className="size-12 object-cover rounded-full"
+                  />
+                </div>
+
+                {/* User info - only visible on larger screens */}
+                <div className="hidden lg:block text-left min-w-0">
+                  <div className="font-medium truncate">{coach.fullname}</div>
+                </div>
+              </button>
+            ))
+          : myPlayers.map((player) => (
+              <button
+                key={player._id}
+                // onClick={() => setSelectedUser(user)}
+                className={`
+                w-full p-3 flex items-center gap-4
+                `}
+                // ${selectedUser?._id === player._id ? "bg-base-300" : ""}
+              >
+                <div className="relative mx-auto lg:mx-0">
+                  <img
+                    src={player.profilePic || "https://cdn-icons-png.flaticon.com/128/149/149071.png"}
+                    alt={player.fullname}
+                    className="size-12 object-cover rounded-full"
+                  />
+                </div>
+
+                {/* User info - only visible on larger screens */}
+                <div className="hidden lg:block text-left min-w-0">
+                  <div className="font-medium truncate">{player.fullname}</div>
+                </div>
+              </button>
+            ))}
+      </div>
+    </aside>
+  );
+};
+export default ChatSidebar;
--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -1,85 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { coachStore, playerStore } from "../store/authStore";
 import { axiosInstance } from "../lib/axios";
 import { Loader } from "lucide-react";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatSidebar from "../components/ChatSidebar";
 
 const Messages = () => {
-  const [myCoaches, setMyCoaches] = useState([]);
-  const [myPlayers, setMyPlayers] = useState([]);
   const [loading, setLoading] = useState(true);
-  const coachX = coachStore((state) => state.coach);
-  const playerX = playerStore((state) => state.player);
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        if (playerX) {
-          const coachesRes = await axiosInstance.get("/player/my-coaches");
-          setMyCoaches(coachesRes.data);
-        }
-        if (coachX) {
-          const playersRes = await axiosInstance.get("/coach/my-players");
-          console.log(playersRes.data);
-          setMyPlayers(playersRes.data);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
 
-  if (loading) {
-    return (
-      <div className="min-h-screen w-full flex items-center justify-center">
-        <Loader className="h-8 w-8 animate-spin" />
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="min-h-screen w-full flex items-center justify-center">
+  //       <Loader className="h-8 w-8 animate-spin" />
+  //     </div>
+  //   );
+  // }
 
   return (
-    <div className="min-h-screen w-full pt-8 px-14">
-      
-      {coachX && (
-        <div>
-          <h1 className="text-center text-4xl font-bold mb-8">My Players</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-            {myPlayers.map((player) => (
-              <div key={player._id} className="bg-neutral-800 p-6 rounded-lg">
-                <div className="flex items-center gap-4">
-                  <img src={player.profilePic || "https://cdn-icons-png.flaticon.com/128/149/149071.png"} className="w-12 h-12 rounded-full"/>
-                  <div>
-                    <h3 className="text-xl font-semibold">{player.fullname}</h3>
-                    <p className="text-sm text-gray-400">{player.rank} • {player.role}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      )}
+    <div className="min-h-screen w-full">
 
-      {playerX && (
-        <div>
-          <h1 className="text-center text-4xl font-bold mb-8">My Coaches</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-            {myCoaches.map((coach) => (
-              <div key={coach._id} className="bg-neutral-800 p-6 rounded-lg">
-                <div className="flex items-center gap-4">
-                  <img src={coach.profilePic} className="w-12 h-12 rounded-full"/>
-                  <div>
-                    <h3 className="text-xl font-semibold">{coach.fullname}</h3>
-                    <p className="text-sm text-gray-400">{coach.rank} • {coach.role}</p>
-                  </div>
-                </div>
-              </div>
-            ))}
+      <div className="h-screen bg-base-200">
+        <div className="flex items-center justify-center pt-20 px-4">
+          <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+            <div className="flex h-full rounded-lg overflow-hidden">
+              <ChatSidebar />
+
+              {/* {!selectedUser ? <NoChatSelected /> : <ChatContainer />} */}
+              <NoChatSelected />
+            </div>
           </div>
         </div>
-      )}
+      </div>
 
     </div>
   );
